20f9eeedb2474d7b480e47b1adb96ba710054e18
Mohd Sameer <mohdsameer1310@gmail.com>
2019-05-08 15:28:44 +0530

    minor fixes for order by filter liting

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,6 @@
 class Invoice < ApplicationRecord
   def self.filter(from, to)
-    where("invoice_date BETWEEN ? AND ?", from, to).order('created_at DESC')
+    where("invoice_date BETWEEN ? AND ?", from, to).order('invoice_date DESC')
   end
 
   def self.total_for(invoice_ids)
